d5eeaf81967898974d3f9c852bcd244fd6df3842
L1092006 <tanloc1092006@gmail.com>
2026-02-01 12:00:57 -0600

    add missing files for the previous commit

---
+++ frontend/restaurant/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+
+export default function Header() {
+  return (
+    <div>
+    <Box bg="green.800">
+        Header
+    </Box>
+    </div>
+  )
+}
