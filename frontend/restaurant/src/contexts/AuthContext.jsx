7e07815f6454828390189a4e54061773d052bc13
L1092006 <tanloc1092006@gmail.com>
2026-02-01 17:10:29 -0600

    add placeholder for authentication context and complete the header

---
+++ frontend/restaurant/src/contexts/AuthContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export default function AuthProvider({ children }) {
+    // FIXME: call the backend to verify if we can still use cookies to log in silently, if not, set log out state.
+    const defaultUser = {
+        username: "Loc",
+        email: "loc@gmail.com"
+    };
+    const [user, setUser] = useState(defaultUser);
+    const [isAuthenticated, setAutheticated] = useState(true);
+
+    //FIXME: implement the login and logout fucntion
+    const login = (username, password) => {
+        setUser(defaultUser);
+        setAutheticated(true);
+    }
+    const logout = () => {
+        setUser(null);
+        setAutheticated(false);
+    }
+
+    return (
+        <AuthContext.Provider value={{user, isAuthenticated, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
