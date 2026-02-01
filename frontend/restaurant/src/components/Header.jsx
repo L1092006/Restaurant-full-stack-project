87194243f5fae8462e83a03260d0e4ea558da761
L1092006 <tanloc1092006@gmail.com>
2026-02-01 15:03:58 -0600

    Style the header, add Restaurant name and cart button without functionality

--- frontend/restaurant/src/components/Header.jsx
+++ frontend/restaurant/src/components/Header.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Flex, Text, Spacer, Avatar, Button, IconButton, Badge } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function Header() {
   return (
     <div>
-    <Box bg="green.800">
-        Header
-    </Box>
+    <Flex bg="green.800" height="5vh" alignItems="center" fontFamily="cursive" gap={2} px="1em">
+        <Link to="/">
+          <Text textStyle="3xl" color="white">The Restaurant</Text>
+        </Link>
+        <Spacer/>
+        <Link to="/cart">
+          <IconButton aria-label='Cart' variant="ghost" colorPalette="white" px="0.5em">
+            <FaShoppingCart/>
+            <Badge color="white" backgroundColor="green.700" borderRadius="full" variant="subtle">1</Badge>
+          </IconButton>     
+        </Link>
+    </Flex>
     </div>
   )
 }
