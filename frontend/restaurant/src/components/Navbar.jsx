04731b086581ad5df0cbae86ae1212788157c33e
L1092006 <tanloc1092006@gmail.com>
2026-02-02 13:53:24 -0600

    add key prop for each Link in Navbar

--- frontend/restaurant/src/components/Navbar.jsx
+++ frontend/restaurant/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link as RouterLink } from "react-router-dom"
 export default function Navbar({ links }) {
     return (
         <Flex alignItems="center" justifyContent="space-around" h="3vh" bg="green.800">
-            {links.map(link => <Link as={RouterLink} color="white" _hover={{ color: "green.500"}} to={link.path}>{link.description}</Link>)}
+            {links.map(link => <Link as={RouterLink} to={link.path} key={link.path} color="white" _hover={{ color: "green.500"}} >{link.description}</Link>)}
         </Flex>
     )
 }
