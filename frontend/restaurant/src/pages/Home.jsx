3cef7fd49ee0bc8bae9c60f5d0e9b8590edb8f29
L1092006 <tanloc1092006@gmail.com>
2026-02-04 00:09:15 -0600

    add all the changes for the previous commit

--- frontend/restaurant/src/pages/Home.jsx
+++ frontend/restaurant/src/pages/Home.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { Flex, Image, Text, Icon } from "@chakra-ui/react"
+import callAPI from "../utils/callAPI"
+import { useEffect, useState } from "react"
+import { LuLoader } from "react-icons/lu";
+import  placeholder from "../assets/img/placeholder.jpg"
 
 export default function Home() {
+  const [featuredBanner, setFeaturedBanner ] = useState(null);
+  const bannerSize = {
+    h: "35vh",
+    w: "75vw"
+  }
+  const bannerColor = "gray.700";
+
+  useEffect(() => {
+    callAPI("featuredBanner").then(res => setFeaturedBanner(res));
+  }, [])
+
   return (
-    <div>Home</div>
+    <Flex mx="6vw"  my="3vh" direction="column" align="center" gap="1vh">
+      <Text alignSelf="start" color={bannerColor} fontSize="5vh" fontFamily="cursive">FEATURED</Text>
+      { featuredBanner ? (
+        <>
+          {featuredBanner.path ? <Image src={featuredBanner.path} h={bannerSize.h} w={bannerSize.w}/> : <Image src={placeholder} h={bannerSize.h} w={bannerSize.w}/>}
+          <Text color={bannerColor} fontSize="2vh" fontFamily="cursive"> {featuredBanner.description} </Text>
+        </>
+      ) : <Icon as={LuLoader} color="gray.400" h={bannerSize.h} w={bannerSize.w}/>}
+    </Flex>
   )
 }
