05184be41ef4317383a223f2f90561f8031399a8
L1092006 <tanloc1092006@gmail.com>
2026-02-21 14:30:48 -0600

    In backend, in settings.py remove session and token authentication, add rotate refresh tokens and blacklist after rotation in simplejwt config

--- backend/restaurantAPI/restaurantAPI/settings.py
+++ backend/restaurantAPI/restaurantAPI/settings.py
@@ -140,8 +140,8 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 
@@ -159,6 +159,8 @@ DJOSER = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
 }
 
 CORS_ALLOWED_ORIGINS = [
