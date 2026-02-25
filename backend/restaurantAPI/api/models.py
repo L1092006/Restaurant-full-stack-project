da75253191b720076674d50f5cb0a605b4c076bf
L1092006 <tanloc1092006@gmail.com>
2026-02-24 22:12:16 -0600

    In backend, create MenuItem and Category models. Migrate them.

--- backend/restaurantAPI/api/models.py
+++ backend/restaurantAPI/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField()
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255, db_index=True, unique=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    featured = models.BooleanField(db_index=True)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
