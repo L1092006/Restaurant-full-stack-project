7b8b43235d669e971ac70df053765fe056447f35
L1092006 <tanloc1092006@gmail.com>
2026-02-26 21:38:40 -0600

    In backend, add stock field to MenuItem model

--- backend/restaurantAPI/api/models.py
+++ backend/restaurantAPI/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 
@@ -9,6 +10,7 @@ class Category(models.Model):
 class MenuItem(models.Model):
     title = models.CharField(max_length=255, db_index=True, unique=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    stock = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     featured = models.BooleanField(db_index=True)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
