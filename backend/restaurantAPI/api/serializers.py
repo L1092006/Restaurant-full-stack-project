168f36def32ba1b646d2b9addb11473cd5b4b2df
L1092006 <tanloc1092006@gmail.com>
2026-02-26 22:13:41 -0600

    In backend, add MenuItemSerializer and CategorySerializer

--- backend/restaurantAPI/api/serializers.py
+++ backend/restaurantAPI/api/serializers.py
@@ -1,7 +1,46 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+from .models import *
+import environ
+from pathlib import Path
+from decimal import Decimal
+
+
+
+# Load environment variables
+BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+env.read_env(BASE_DIR / '.env')
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['id', 'username', 'first_name', 'last_name', 'email']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'title', 'slug']
+        extra_kwargs = {
+            'slug': {'read_only': True}
+        }
+
+    def create(self, validated_data):
+        title = validated_data.get('title')
+        slug = slugify(title)
+        validated_data['slug'] = slug
+        return super().create(validated_data)
+    
+class MenuItemSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.IntegerField(write_only=True)
+    price_after_tax = serializers.SerializerMethodField(method_name='getPriceAfterTax')
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'title', 'category', 'category_id', 'price', 'price_after_tax' 'stock', 'featured', 'description', 'image_paths']
+
+    def getPriceAfterTax(self, item):
+        tax = Decimal(env('TAX'))
+        return item.price * tax
