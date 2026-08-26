966aa943c02ac9d4e486a6c24163b6b8c78e6f64
L1092006 <tanloc1092006@gmail.com>
2026-08-26 09:39:01 -0500

    In frontend, fix the empty cats handling in menu page

--- frontend/restaurant/src/pages/Menu.jsx
+++ frontend/restaurant/src/pages/Menu.jsx
@@ -92,21 +92,21 @@ export default function Menu() {
         return <Text color={tabStyle.color} fontFamily={tabStyle.fontFamily}>Loading</Text>
     }
 
-    return (
-        <Tabs.Root defaultValue={categories[0].id} fitted minH={mainSize} colorPalette="green" fontFamily={tabStyle.fontFamily} color={tabStyle.color}>
+    return categories && (
+        <Tabs.Root defaultValue={categories.length > 0 ? categories[0].id : undefined} fitted minH={mainSize} colorPalette="green" fontFamily={tabStyle.fontFamily} color={tabStyle.color}>
             <Tabs.List bg="gray.300" >
-                {categories.map(cat => {
+                {categories && categories.map(cat => {
                     return (
                         <Tabs.Trigger value={cat.id} key={cat.id} color={tabStyle.color}>{cat.title}</Tabs.Trigger>
                     )
                 })}
             </Tabs.List>
 
-            {categories.map(cat => {
+            {categories && categories.map(cat => {
                 return (
                     <Tabs.Content value={cat.id} key={cat.id} >
                         <Grid templateColumns="repeat(auto-fit, 20rem)"  justifyContent="center" gap="2rem" px="4vw" pt="1rem" pb="2rem">
-                            {itemsInCat.get(cat.id).filter(item => item.stock > 0).map(item => (
+                            {itemsInCat && itemsInCat.get(cat.id) && itemsInCat.get(cat.id).filter(item => item.stock > 0).map(item => (
                                 <Link to={`/menu/${item.id}`} key={item.id}>
                                     <Card.Root  color={tabStyle.color} colorPalette="white" _hover={{shadow: "lg"}}> 
                                         <Image src={item.path ? item.path : placeholder} aspectRatio={6/4}/>
@@ -123,7 +123,7 @@ export default function Menu() {
                                                 <IoMdAdd/>
                                                 </IconButton>
                                             </HStack>
-                                            {(item.stock < 5) ? <Text color="red.500">Warning: Low stock. Only {item.stock} items in stock.</Text> : ''}
+                                            {itemsInCat && (item.stock < 5) ? <Text color="red.500">Warning: Low stock. Only {item.stock} items in stock.</Text> : ''}
                                         </Card.Body>
                                     </Card.Root>
                                 </Link>
